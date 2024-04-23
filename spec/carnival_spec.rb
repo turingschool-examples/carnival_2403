07ad579277ae86c48ef3dbcb06ada71bb3c48e2a
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-23 10:59:01 -0600

    test: total revenue test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -18,16 +18,30 @@ RSpec.describe Carnival do
   end
 
   describe 'rides' do
+    before do
+      @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+      @carnival1.add_ride(@ride1)
+      @carnival1.add_ride(@ride2)
+      @carnival1.add_ride(@ride3)
+      @visitor1 = Visitor.new('Bruce', 54, '$10')
+      @visitor2 = Visitor.new('Tucker', 36, '$5')
+
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+      @ride3.board_rider(@visitor3)
+      @ride3.board_rider(@visitor3)
+    end
+
     it 'can add rides to the rides array' do
-      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
-      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
-      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
-
-      @carnival1.add_ride(ride1)
-      @carnival1.add_ride(ride2)
-      @carnival1.add_ride(ride3)
-
       expect(@carnival1.rides).to eq([ride1, ride2, ride3])
+    end
+
+    it 'can calculate total revenue' do
+      expect(@carnival1.total_revenue).to eq(6)
     end
   end
 end
