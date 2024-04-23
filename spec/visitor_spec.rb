7a9bda31b8ee4ee092626618ed95e14983f159bb
Zach Bergman <bzach634@gmail.com>
2024-04-23 09:11:26 -0600

    test: Add #add_preferences test

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -28,4 +28,15 @@ RSpec.describe Visitor do
       expect(@visitor1.preferences).to eq []
     end
   end
+
+  describe "#add_preference" do
+    it "adds a preference to the preferences list" do
+      expect(@visitor1.preferences).to eq []
+
+      @visitor1.add_preference(:gentle)
+      @visitor1.add_preference(:thrilling)
+
+      expect(@visitor1.preferences).to eq [:gentle, :thrilling]
+    end
+  end
 end
