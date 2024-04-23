33e6fb4da1270f064cf03ecf152ce9813856fdad
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-23 09:05:20 -0600

    test: initialization

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,1 +1,13 @@
 require "./spec/spec_helper"
+
+RSpec.describe Visitor do
+  before do
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+  end
+
+  describe 'initialize' do
+    it 'can create a visitor object' do
+      expect(@visitor1).to be_a(Visitor)
+    end
+  end
+end
