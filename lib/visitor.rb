9196a59afa7a4ce96c008bfa9955207a74f53e8a
Zach Bergman <bzach634@gmail.com>
2024-04-23 09:12:06 -0600

    feat: Add #add_preferences

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -3,11 +3,15 @@ class Visitor
               :height, 
               :spending_money, 
               :preferences
-              
+
   def initialize(name, height, spending_money)
     @name = name
     @height = height
     @spending_money = spending_money.delete("$").to_i
     @preferences = []
+  end
+
+  def add_preference(preference)
+    @preferences << preference
   end
 end
