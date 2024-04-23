78fa4764331a7cfd640b36046008a64cd4bf64c8
Lance Nelson <lancen04@gmail.com>
2024-04-23 11:11:18 -0400

    created visitor class and an accompanying spec file

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,1 +1,23 @@
 require "./spec/spec_helper"
+
+RSpec.describe Visitor do
+    it 'exists' do
+        visitor1 = Visitor.new('Bruce', 54, '$10')    
+        expect(visitor1).to be_a Visitor
+    end
+
+    it 'has a name' do
+        visitor1 = Visitor.new('Bruce', 54, '$10') 
+        expect(visitor1.name).to eq 'Bruce'
+    end
+
+    it 'has a height' do
+        visitor1 = Visitor.new('Bruce', 54, '$10') 
+        expect(visitor1.height).to eq(54)
+    end
+
+    it 'has spending money' do
+        visitor1 = Visitor.new('Bruce', 54, '$10') 
+        expect(visitor1.spending_money).to eq '$10'
+    end
+end
