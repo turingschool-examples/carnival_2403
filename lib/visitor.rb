41ad3b059d9332b64f0dc437c0c654666083aa70
Clyde <clyde.autin@gmail.com>
2024-04-23 08:13:15 -0700

    visitor class initialized

---
+++ lib/visitor.rb
@@ -0,0 +1,12 @@
+class Visitor
+
+    attr_reader :name, :height, :spending_money, :preferences
+
+    def initialize(name, height, spending_money)
+        @name = name
+        @height = height
+        @spending_money = spending_money
+        @preferences = []
+    end
+    
+end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,1 +1,28 @@
 require "./spec/spec_helper"
+require "./lib/visitor.rb"
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
+
+RSpec.describe Visitor do
+
+    it 'Initializes' do
+        visitor1 = Visitor.new('Bruce', 54, '$10')
+        visitor2 = Visitor.new('Tucker', 36, '$5')
+        visitor3 = Visitor.new('Penny', 64, '$15')
+
+        expect(visitor1).to be_an_instance_of(Visitor)
+    end
+
+    it 'can add preferences' do
+        visitor1 = Visitor.new('Bruce', 54, '$10')
+        visitor2 = Visitor.new('Tucker', 36, '$5')
+        visitor3 = Visitor.new('Penny', 64, '$15')
+
+        visitor1.add_preference(:gentle)
+        visitor1.add_preference(:thrilling)
+
+        expect(visitor1.preferences).to eq([:gentle, :thrilling])
+    end
+end
