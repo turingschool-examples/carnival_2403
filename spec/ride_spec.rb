13df07f1fd4280494e81b3e5778412a2c7b705ae
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-23 09:28:55 -0600

    test: ride init test

---
+++ spec/ride_spec.rb
@@ -0,0 +1,20 @@
+require "./spec/spec_helper"
+
+RSpec.describe Ride do
+  before do
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+  end
+
+  describe 'initialize' do
+    it 'can initialize a ride object' do
+      expect(@ride1).to be_a(Ride)
+    end
+
+    it 'has attributes' do
+      expect(@ride1.name).to eq("Carousel")
+      expect(@ride1.min_height).to eq(24)
+      expect(@ride1.admission_fee).to eq(1)
+      expect(@ride1.excitement).to eq(:gentle)
+    end
+  end
+end
