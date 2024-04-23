8db6fea3a5a7f267586d2167fa899f3b84a1a9a6
Clyde <clyde.autin@gmail.com>
2024-04-23 08:29:15 -0700

    ride class initializes

---
+++ lib/ride.rb
@@ -0,0 +1,14 @@
+class Ride 
+
+    attr_reader :name, 
+                :min_height, 
+                :admission_fee,
+                :excitement 
+
+    def initialize(ride_details)
+        @name = [name:]
+        @min_height = [min_height:]
+        @admission_fee = [admission_fee:]
+        @excitement = [excitement:]
+    end
+end
