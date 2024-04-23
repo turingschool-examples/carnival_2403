da676ed8d15d29ab27f2a8010102823a9a97ad59
Lance Nelson <lancen04@gmail.com>
2024-04-23 11:58:36 -0400

    created board rider method and corresponding test

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,12 +1,24 @@
 class Ride
-    attr_reader :name, :min_height, :admission_fee, :excitement
+    attr_reader :name, 
+                :min_height, 
+                :admission_fee, 
+                :excitement,
+                :total_revenue,
+                :rider_log
 
     def initialize(data)
         @name = data[:name]
         @min_height = data[:min_height]
         @admission_fee = data[:admission_fee]
         @excitement = data[:excitement]
-        @rider_log = {}
+        @rider_log = Hash.new(0)
+        @total_revenue = 0
+    end
+
+    def board_rider(visitor)
+        # add visitor to rider log
+        @rider_log[visitor] = @rider_log[visitor] += 1
+        # require 'pry'; binding.pry
     end
 
 end
