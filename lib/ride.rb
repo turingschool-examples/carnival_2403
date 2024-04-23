55ffd847977b5612116274a2d1054a82e1398359
Zach Bergman <bzach634@gmail.com>
2024-04-23 09:29:21 -0600

    feat: Add readable attributes

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,5 +1,15 @@
 class Ride
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue
+              
   def initialize(details)
-    
+    @name = details[:name]
+    @min_height = details[:min_height]
+    @admission_fee = details[:admission_fee]
+    @excitement = details[:excitement]
+    @total_revenue = 0
   end
 end
