2b1f56e20d96b2bee184c09fd7965de09c61253b
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-23 10:37:10 -0600

    add subrtract_spending_money to Visitor

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -52,4 +52,12 @@ RSpec.describe Visitor do
             expect(@visitor1.tall_enough?(64)).to be false
         end
     end
+
+    describe "#subtract_spending_money" do
+        it "can subtract from spending money" do
+            expect(@visitor1.spending_money).to eq(10)
+
+            expect(@visitor1.subtract_spending_money(2)).to eq 8
+        end
+    end
 end
