cd6c91b489e59b110fa5efdfda764d4572c15bfc
Clyde <clyde.autin@gmail.com>
2024-04-23 08:14:53 -0700

    It can add preferences

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -8,5 +8,9 @@ class Visitor
         @spending_money = spending_money
         @preferences = []
     end
-    
+
+    def add_preference(preference)
+        @preferences << preference
+    end
+
 end
