335aeeff973f17b3ef7fac84eabbc61d8f982b33
Clyde <clyde.autin@gmail.com>
2024-04-23 08:23:47 -0700

    ride spec file created with init test

---
+++ spec/ride_spec.rb
@@ -0,0 +1,10 @@
+require 'spec/spec_helper'
+
+RSpec.describe Ride do
+
+    it 'initializes' do
+        ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+        expect(ride1).to be_an_instance_of(Ride)
+    end
+end
