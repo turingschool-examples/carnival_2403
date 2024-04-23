385ddbc7a046ced0eae40667aaaa872b04ed0efb
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-23 11:52:56 -0600

    test: summary of a carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -72,4 +72,54 @@ RSpec.describe Carnival do
       expect(@carnival1.most_profitable_ride).to eq(@ride3)
     end
   end
+
+  describe 'summary' do
+    it 'can provide a summary of the carnival' do
+      @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+      @carnival1.add_ride(@ride1)
+      @carnival1.add_ride(@ride2)
+      @carnival1.add_ride(@ride3)
+      @visitor1 = Visitor.new('Bruce', 54, '$10')
+      @visitor3 = Visitor.new('Penny', 64, '$15')
+      @visitor3.add_preference(:thrilling)
+      @visitor1.add_preference(:gentle)
+
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor1)
+      @ride3.board_rider(@visitor3)
+      @ride3.board_rider(@visitor3)
+      expected = ({
+        visitor_count: 2,
+        revenue_earned: 6,
+        visitors: 
+          {
+            visitor: @visitor1,
+            favorite_ride: @ride1,
+            total_money_spent: 3
+          },
+          {
+            visitor: @visitor3,
+            favorite_ride: @ride3,
+            total_money_spent: 4
+          }
+        rides: [
+          {
+            ride: @ride1,
+            riders: [@visitor1]
+            total_revenue: 3
+          },
+          {
+            ride: @ride3,
+            riders: [@visitor3]
+            total_revenue: 4
+          }]
+      })
+
+      expect(@carnival1.summary).to eq(expected)
+    end
+  end
 end
