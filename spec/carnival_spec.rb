e1018654e80b173c78fc47d68f51320bad981902
Zach Bergman <bzach634@gmail.com>
2024-04-23 11:02:01 -0600

    refactor: Add to #total_revenue test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -74,13 +74,18 @@ RSpec.describe Carnival do
       @carnival.add_ride(@ride3)
 
       @visitor1.add_preference(:gentle)
+      @visitor2.add_preference(:gentle)
+      @visitor3.add_preference(:thrilling)
 
       @ride1.board_rider(@visitor1)
       @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor1)
 
-      @ride2.board_rider(@visitor1)
+      @ride2.board_rider(@visitor2)
 
-      expect(@carnival.total_revenue).to eq 7
+      @ride3.board_rider(@visitor3)
+
+      expect(@carnival.total_revenue).to eq 10
     end
   end
 end
