8299e20bcfdb49d15ad3229eef1437c4b7f56512
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-23 10:50:47 -0600

    test: carnival init

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -2,12 +2,18 @@ require "./spec/spec_helper"
 
 RSpec.describe Carnival do
   before do
-    carnival1 = Carnival.new("Boulder Carnival")
+    @carnival1 = Carnival.new("Boulder Carnival", 14)
   end
 
   describe 'initialize' do
     it 'can create a carnival object' do
-      expect(carnival1).to be_a(Carnival)
+      expect(@carnival1).to be_a(Carnival)
+    end
+
+    it 'has attributes' do
+      expect(@carnival1.name).to eq("Boulder Carnival")
+      expect(@carnival1.duration).to eq(14)
+      expect(@carnival1.rides).to eq([])
     end
   end
 end
