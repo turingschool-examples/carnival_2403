f58feab0bd108763e068cf5b7b19da8c47cefe48
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-23 10:52:57 -0600

    test: add ride method

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe Carnival do
       expect(@carnival1.rides).to eq([])
     end
   end
+
+  describe 'rides' do
+    it 'can add rides to the rides array' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+      @carnival1.add_ride(ride1)
+      @carnival1.add_ride(ride2)
+      @carnival1.add_ride(ride3)
+
+      expect(@carnival1.rides).to eq([ride1, ride2, ride3])
+    end
+  end
 end
