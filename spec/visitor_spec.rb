5a7a37cfa578638f3af6781562b6b48049dae032
Zach Bergman <bzach634@gmail.com>
2024-04-23 09:05:13 -0600

    test: Add setup/Add #initialize test

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,1 +1,15 @@
 require "./spec/spec_helper"
+
+RSpec.describe Visitor do
+  before(:each) do
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+  end
+
+  describe "#initialize" do
+    it "exists" do
+      expect(@visitor1).to be_instance_of Visitor
+    end
+  end
+end
