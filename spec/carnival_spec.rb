0b7e94a604a47c6b43ef7a75228ada5ddd124887
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-23 11:35:31 -0600

    Add test for total_revenue in carnvial

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,4 +1,5 @@
 require "./spec/spec_helper"
+require "pry"
 
 RSpec.describe Carnival do
     before(:all) do 
@@ -27,19 +28,57 @@ RSpec.describe Carnival do
     end
 
     describe "#add_ride" do
-        it "can initialize" do
+        it "can add rides to a carnvial" do
             expect(@carnival1.rides).to eq ([])
 
             @carnival1.add_ride(@ride1)
             expect(@carnival1.rides).to eq ([@ride1])
 
-            @carnival1.add_ride(@ride2)
-            expect(@carnival1.rides).to eq ([@ride1, @ride2])
+            @carnival1.add_ride(@ride3)
+            expect(@carnival1.rides).to eq ([@ride1, @ride3])
 
             @carnival2.add_ride(@ride3)
             expect(@carnival2.rides).to eq ([@ride3])
 
-            expect(@carnival1.rides).to eq ([@ride1, @ride2])
+            expect(@carnival1.rides).to eq ([@ride1, @ride3])
+        end
+    end
+
+    describe "#total_revenue" do
+        it "can calculate the total revenue for a carnival" do
+            expect(@ride1.rider_log).to eq({})
+            expect(@ride1.total_revenue).to eq (0)
+
+            @visitor1.add_preference(:gentle)
+            @visitor2.add_preference(:gentle)
+
+            @ride1.board_rider(@visitor1)
+            @ride1.board_rider(@visitor2)
+            @ride1.board_rider(@visitor1)
+
+            expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
+
+            expect(@visitor1.spending_money).to eq (8)
+            expect(@visitor2.spending_money).to eq (4)
+
+            expect(@ride1.total_revenue).to eq (3)
+            
+            @visitor2.add_preference(:thrilling)
+            @visitor3.add_preference(:thrilling)
+
+            @ride3.board_rider(@visitor1)
+            @ride3.board_rider(@visitor2)
+            @ride3.board_rider(@visitor3)
+
+            expect(@visitor1.spending_money).to eq (8)
+            expect(@visitor2.spending_money).to eq (4)
+            expect(@visitor3.spending_money).to eq (13)
+
+            expect(@ride3.total_revenue).to eq (2)
+            expect(@ride3.rider_log).to eq({@visitor3 => 1})
+            
+            expect(@carnival1.total_revenue).to eq (5)
+            expect(@carnival2.total_revenue).to eq (2)
         end
     end
 
