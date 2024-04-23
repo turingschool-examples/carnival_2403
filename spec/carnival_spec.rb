bcc6c31f77fff8d3fae2213beb415c4c5cdb594c
Zach Bergman <bzach634@gmail.com>
2024-04-23 10:23:16 -0600

    test: Add Visitor Objects to setup/Add #most_popular_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -18,6 +18,10 @@ RSpec.describe Carnival do
       min_height: 54, 
       admission_fee: 2, 
       excitement: :thrilling })
+
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
   end
 
   describe "#initialize" do
@@ -43,6 +47,22 @@ RSpec.describe Carnival do
       @carnival.add_ride(@ride3)
 
       expect(@carnival.rides).to eq [@ride1, @ride2, @ride3]
+    end
+  end
+
+  describe "#most_popular_ride" do
+    it "returns the Ride object that has been ridden the most" do
+      @carnival.add_ride(@ride1)
+      @carnival.add_ride(@ride2)
+      @carnival.add_ride(@ride3)
+
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor3)
+
+      @ride2.board_rider(@visitor1)
+
+      expect(@carnival.most_popular_ride).to eq @ride1
     end
   end
 end
