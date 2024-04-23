0e43a828b9499ed27f4b5bfbf5a4c9bcc2b65ca9
Austin Kenny <austinkennywork@gmail.com>
2024-04-23 11:21:39 -0400

    Added Visitor Class with initialization and preference method

---
+++ lib/visitor.rb
@@ -0,0 +1,15 @@
+class Visitor
+    attr_reader :name, :height, :spending_money, :preferences
+
+    def initialize(name, height, spending_money)
+        @name = name
+        @height = height
+        @spending_money = spending_money.delete("$").to_i
+        @preferences = []
+    end
+
+    def add_preference(preference)
+        preferences << preference
+    end
+    
+end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,1 +1,21 @@
 require "./spec/spec_helper"
+
+describe Visitor do
+    describe "initialization" do
+        it "can create a new Visitor" do
+            visitor1 = Visitor.new("Bruce", 54, "$10")
+            expect(visitor1.name).to eq("Bruce")
+            expect(visitor1.height).to eq(54)
+            expect(visitor1.spending_money).to eq(10)
+        end
+    end
+
+    describe "add_preference" do
+        it "can add a preference" do
+            visitor1 = Visitor.new("Bruce", 54, "$10")
+            visitor1.add_preference(:gentle)
+            visitor1.add_preference(:thrilling)
+            expect(visitor1.preferences).to eq([:gentle, :thrilling])
+        end
+    end
+end
