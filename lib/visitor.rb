b1dac3f17cf63e10ea0f846b46147a721db7559f
James McHugh <jdmchugh111@gmail.com>
2024-04-23 11:24:00 -0400

    Wrote and passed test for Visitor intialize

---
+++ lib/visitor.rb
@@ -0,0 +1,26 @@
+
+class Visitor
+    attr_reader :name,
+                :height,
+                :spending_money,
+                :preferences
+
+    def initialize(name, height, spending_money)
+        @name = name
+        @height = height
+        @spending_money = spending_money.delete("$").to_i
+        @preferences = []
+    end
+
+    def add_preference(preference)
+        @preferences << preference
+    end
+
+    def tall_enough?(min_height)
+        if min_height <= @height
+            true
+        else
+            false
+        end
+    end
+end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,1 +1,33 @@
 require "./spec/spec_helper"
+
+RSpec.describe Visitor do
+    before(:each) do
+        @visitor1 = Visitor.new('Bruce', 54, '$10')
+        @visitor2 = Visitor.new('Tucker', 36, '$5')
+        @visitor3 = Visitor.new('Penny', 64, '$15')
+    end
+
+    describe "#initialize" do
+        it "exists" do
+            expect(@visitor1).to be_a(Visitor)
+        end
+
+        it "has a name" do
+            expect(@visitor1.name).to eq("Bruce")
+        end
+
+        it "has a height" do
+            expect(@visitor1.height).to eq(54)
+        end
+
+        it "has spending money as integer" do
+            expect(@visitor1.spending_money).to eq(10)
+        end
+
+        it "has preferences - default is empty bracket" do
+            expect(@visitor1.preferences).to eq([])
+        end
+    end
+
+    
+end
