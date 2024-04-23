3395f52af33c1ba7c0ba349adb93e1f98816d548
Zach Bergman <bzach634@gmail.com>
2024-04-23 09:09:37 -0600

    feat: Add readable attributes

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,5 +1,13 @@
 class Visitor
+  attr_reader :name, 
+              :height, 
+              :spending_money, 
+              :preferences
+              
   def initialize(name, height, spending_money)
-    
+    @name = name
+    @height = height
+    @spending_money = spending_money.delete("$").to_i
+    @preferences = []
   end
 end
