d123314ebdfb9f64e86b737eb9bf9f8970c175f5
Zach Bergman <bzach634@gmail.com>
2024-04-23 09:27:34 -0600

    test: Add readable attributes test

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -26,5 +26,25 @@ RSpec.describe Ride do
     it "exists" do
       expect(@ride1).to be_instance_of Ride
     end
+
+    it "has a name" do
+      expect(@ride1.name).to eq "Carousel"
+    end
+
+    it "has a min_height" do
+      expect(@ride1.min_height).to eq 24
+    end
+
+    it "has an admission fee" do
+      expect(@ride1.admission_fee).to eq 1
+    end
+
+    it "has an excitement level" do
+      expect(@ride1.excitement).to eq :gentle
+    end
+
+    it "can track total revenue" do
+      expect(@ride1.total_revenue).to eq 0
+    end
   end
 end
