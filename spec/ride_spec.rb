77a36403aeb5264d0c00ebd7565499437284dfcb
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-23 09:57:14 -0600

    Add initialize test for Ride class

---
+++ spec/ride_spec.rb
@@ -0,0 +1,33 @@
+require "./spec/spec_helper"
+
+RSpec.describe Ride do
+    before(:all) do 
+        @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    end
+
+    describe "#initialize" do
+        it "can initialize" do
+            expect(@ride1).to be_an_instance_of (Ride)
+        end
+
+        it "can initialize wtih a name" do
+            expect(@ride1.name).to eq('Carousel')
+        end
+
+        it "can initialize wtih a minimum height" do
+            expect(@ride1.min_height).to eq(24)
+        end
+
+        it "can initialize wtih an admission fee" do
+            expect(@ride1.admission_fee).to eq(1)
+        end
+
+        it "can initialize wtih excitement level" do
+            expect(@ride1.excitement).to eq(:gentle)
+        end
+        
+        it "can initialize wtih history of riders" do
+            expect(@ride1.rider_log).to eq({})
+        end
+    end
+end
