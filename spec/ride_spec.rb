a301f0be2b9a98f1db039166454c4a55cef47e92
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-23 10:41:34 -0600

    Add tests for board_rider method

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,6 +3,9 @@ require "./spec/spec_helper"
 RSpec.describe Ride do
     before(:all) do 
         @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+        @visitor1 = Visitor.new('Bruce', 54, '$10')
+        @visitor2 = Visitor.new('Tucker', 36, '$5')
+        @visitor3 = Visitor.new('Penny', 64, '$15')
     end
 
     describe "#initialize" do
@@ -29,12 +32,31 @@ RSpec.describe Ride do
         it "can initialize wtih history of riders" do
             expect(@ride1.rider_log).to eq({})
         end
-        
-    end
-    
-    describe "#total_revenue" do
+
         it "can return total_revenue" do
             expect(@ride1.total_revenue).to eq (0)
+        end
+        
+    end
+
+    describe "#board_rider" do
+        it "can board elgible riders" do
+            expect(@ride1.rider_log).to eq({})
+            expect(@ride1.total_revenue).to eq (0)
+
+            @visitor1.add_preference(:gentle)
+            @visitor2.add_preference(:gentle)
+
+            @ride1.board_rider(@visitor1)
+            @ride1.board_rider(@visitor2)
+            @ride1.board_rider(@visitor1)
+
+            expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
+
+            expect(@visitor1.spending_money).to eq (8)
+            expect(@visitor2.spending_money).to eq (4)
+
+            expect(@ride1.total_revenue).to eq (3)
         end
     end
 end
