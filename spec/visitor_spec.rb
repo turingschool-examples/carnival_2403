551e966c059b21259c2d2a7aa239a54354256fc9
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-23 09:21:57 -0600

    test: height check test

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -29,4 +29,16 @@ RSpec.describe Visitor do
       expect(@visitor1.preferences).to eq([:gentle, :thrilling])
     end
   end
+
+  describe 'height check' do
+    it 'can check to see if a visitor is tall enough for a ride' do
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+
+      expect(@visitor1.tall_enough(54)).to be(true)
+      expect(visitor2.tall_enough(54)).to be(false)
+      expect(visitor3.tall_enough(54)).to be(true)
+      expect(@visitor1.tall_enough(64)).to be(false)
+    end
+  end
 end
