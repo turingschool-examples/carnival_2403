f6fd81a1f91a51b71213f80645c94b10d03206ba
Chee Lee <cheeleertr@gmail.com>
2024-04-23 10:12:24 -0500

    Add Visitor initialize and add preferences test/methods

---
+++ lib/visitor.rb
@@ -0,0 +1,19 @@
+class Visitor
+  attr_reader :name,
+              :height,
+              :spending_money,
+              :preferences
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money.delete("$").to_f
+    @preferences = []
+  end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+
+  
+end
