ccdb86d49323ce6dc0c38cb51d1a770fb840b79f
Clyde <clyde.autin@gmail.com>
2024-04-23 09:27:47 -0700

    Carnival can now add rides

---
+++ lib/carnival.rb
@@ -0,0 +1,13 @@
+class Carnival
+
+    attr_reader :duration, :rides
+
+    def initialize(duration)
+        @duration = duration
+        @rides = []
+    end
+
+    def add_ride(ride)
+        @rides <<ride
+    end
+end
--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Carnival do
 
         expect(carnival_A).to be_an_instance_of Carnival
     end
+
+    it 'can add rides' do
+        carnival_A = Carnival.new(14)
+        ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+        ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+        ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+        carnival_A.add_ride(ride1)
+
+        expect(carnival_A.rides).to eq([ride1])
+    end
 end
