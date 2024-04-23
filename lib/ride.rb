efe4d601cc3da4a320a282367efa35f41c799189
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-23 09:34:11 -0600

    feat: total revenue method

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,12 +2,14 @@ class Ride
   attr_reader :name, 
               :min_height, 
               :admission_fee, 
-              :excitement
+              :excitement,
+              :total_revenue
 
   def initialize(ride_attributes)
     @name = ride_attributes[:name]
     @min_height = ride_attributes[:min_height]
     @admission_fee = ride_attributes[:admission_fee]
     @excitement = ride_attributes[:excitement]
+    @total_revenue = 0
   end
 end
