36da0aab134b6cd5e98cc332ff862a046a75973d
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-23 10:37:35 -0600

    Add subtract_spending_money to Visitor

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -6,7 +6,7 @@ class Visitor
     def initialize(name, height, spending_money)
         @name = name
         @height = height
-        @spending_money = spending_money.delete("$").to_f
+        @spending_money = spending_money.delete("$").to_i
         @preferences = []
     end
 
@@ -16,6 +16,10 @@ class Visitor
 
     def tall_enough?(required_height)
         required_height <= @height
+    end
+
+    def subtract_spending_money(amount)
+        @spending_money -= amount
     end
 end
 
